1eb7ad9a67e4ba5adc64d748965b9579d5ee7b5c
Netto Cavalcante <frcnetto@gmail.com>
2017-07-22 11:48:54 -0300

    gitinit

---
+++ app/src/main/java/br/com/frcnetto/chatapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.frcnetto.chatapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.frcnetto.chatapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/br/com/frcnetto/chatapp/model/Mensagem.java
@@ -0,0 +1,8 @@
+package br.com.frcnetto.chatapp.model;
+
+/**
+ * Created by frcnetto on 21/07/17.
+ */
+
+public class Mensagem {
+}
